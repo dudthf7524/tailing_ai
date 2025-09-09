fc0a598426ce79ed6706fa8c5326e2f5c7a0b997
user <dudthf7524@naver.com>
2025-09-09 18:01:58 +0900

    update

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from config import config
 def main():
     # uvicorn.run("app.app:app", host="localhost", port=config.PORT, reload=True)
     
-  uvicorn.run("app.app:app", host="175.106.99.173", port=config.PORT, reload=False)
+  uvicorn.run("app.app:app", host="0.0.0.0", port=config.PORT, reload=False)
 
 if __name__ == "__main__":
     main()
